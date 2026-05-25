127dcb6e18f5c7ca8e967ca4c59f84869019e29b
shinghamshivam108-stack <shinghamshivam108@gmail.com>
2026-05-25 14:51:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-
-document.querySelector(".btn")
+document
+.getElementById("enterBtn")
 .addEventListener("click", function(){
 
-    alert("Surprise coming soon ❤️");
+document.querySelector(".container")
+.style.display="none";
+
+document
+.getElementById("secretPage")
+.style.display="flex";
+
+});
+
+document
+.getElementById("musicBtn")
+.addEventListener("click", function(){
+
+alert("Music feature coming next 🎵");
 
 });
